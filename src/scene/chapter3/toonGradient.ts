404adbf9dbd2d40549e54a90f2dcf8451966ce28
Sylvain <sylvain.maurier@gmail.com>
2026-06-20 16:16:00 +0200

    fix: toonGradient add minFilter NearestFilter

--- src/scene/chapter3/toonGradient.ts
+++ src/scene/chapter3/toonGradient.ts
@@ -3,4 +3,5 @@ import * as THREE from 'three'
 const colors = new Uint8Array([40, 128, 255])
 export const toonGradient = new THREE.DataTexture(colors, 3, 1, THREE.RedFormat)
 toonGradient.magFilter = THREE.NearestFilter
+toonGradient.minFilter = THREE.NearestFilter
 toonGradient.needsUpdate = true
